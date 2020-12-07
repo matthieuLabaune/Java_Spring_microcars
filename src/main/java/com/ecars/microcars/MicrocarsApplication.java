e64529964c93c6b9494a6a40d995a03aec75cccc
Matthieu <matthieu.labaune@le-campus-numerique.fr>
2020-12-07 10:40:27 +0100

    [INIT] · Initialize project microcars

---
+++ src/main/java/com/ecars/microcars/MicrocarsApplication.java
@@ -0,0 +1,13 @@
+package com.ecars.microcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrocarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecars/microcars/MicrocarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecars.microcars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrocarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
